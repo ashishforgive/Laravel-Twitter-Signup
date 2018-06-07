766a2fae3a4f1a5c65abb7a1f07793d50b5c824c
Ashish Kumar <akmashish15@gmail.com>
2018-06-08 01:35:10 +0530

    First commit

---
+++ app/SocialTwitterAccount.php
@@ -0,0 +1,17 @@
+<?php
+
+// SocialTwitterAccount.php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SocialTwitterAccount extends Model
+{
+  protected $fillable = ['user_id', 'provider_user_id', 'provider'];
+
+  public function user()
+  {
+      return $this->belongsTo(User::class);
+  }
+}
